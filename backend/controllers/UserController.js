0a178debd5250411187398c2be04e2f1af94661b
Daniel Cardoso <cardoso.daniel2612@gmail.com>
2023-04-18 21:44:11 -0300

    Validações de usuários

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,45 @@ const User = require('../models/User')
 
 module.exports = class UserController {
     static async register(req, res) {
-        res.json('Olá Get A Pet')
+        const { name, email, phone, password, confirmpassword } = req.body
+
+        // validations: Fazer uma série de validações, pois os dados são obrigatórios e é necessário enviar uma resposta de erro para o usuário.
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório!' })
+            return
+        }
+
+        if (!email) {
+            res.status(422).json({ message: 'O e-mail é obrigatório!' })
+            return
+        }
+
+        if (!phone) {
+            res.status(422).json({ message: 'O telefone é obrigatório!' })
+            return
+        }
+
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatória!' })
+            return
+        }
+
+        if (!confirmpassword) {
+            res.status(422).json({ message: 'A confirmação de senha é obrigatória!' })
+            return
+        }
+
+        if (password !== confirmpassword) {
+            res.status(422).json({ message: 'A senha e a confirmação de senha precisam ser iguais!' })
+            return
+        }
+
+        // check if user exists
+        const userExists = await User.findOne({ email: email})
+
+        if (userExists) {
+            res.status(422).json({ message: 'Por favor, utilize outro e-mail!'})
+            return
+        }
     }
 }
