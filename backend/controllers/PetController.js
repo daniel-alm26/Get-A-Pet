a1a1fc077f4a9678a553687019906371a882f600
Daniel Cardoso <cardoso.daniel2612@gmail.com>
2023-05-03 18:06:52 -0300

    Resgatando Pet pelo id

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -3,6 +3,7 @@ const Pet = require('../models/Pet')
 // helpers
 const getToken = require('../helpers/get-token')
 const getUserByToken = require('../helpers/get-user-by-token')
+const ObjectId = require('mongoose').Types.ObjectId
 
 module.exports = class PetController {
     // create a pet
@@ -99,6 +100,23 @@ module.exports = class PetController {
         const pets = await Pet.find({ 'adopter._id': user._id }).sort('createdAt')
 
         res.status(200).json({ pets })
+    }
 
+    static async getPetById(req, res) {
+        const id = req.params.id
+
+        if (!ObjectId.isValid(id)) {
+            res.status(422).json({ message: 'ID inválido!' })
+            return
+        }
+
+        // check if pet exists
+        const pet = await Pet.findOne({ _id: id })
+
+        if (!pet) {
+            res.status(404).json({ message: 'Pet não encontrado!' })
+        }
+
+        res.status(200).json({ pet: pet })
     }
 }
