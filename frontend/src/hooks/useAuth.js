b3a3587eaa5984f3b08d70cbe875da8ef91b7185
Daniel Cardoso <cardoso.daniel2612@gmail.com>
2023-05-15 11:24:34 -0300

    Função de login

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -12,7 +12,7 @@ export default function useAuth() {
     useEffect(() => {
         const token = localStorage.getItem('token')
 
-        if(token) {
+        if (token) {
             api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
             setAuthenticated(true)
         }
@@ -24,6 +24,24 @@ export default function useAuth() {
 
         try {
             const data = await api.post('/users/register', user).then((response) => {
+                return response.data
+            })
+
+            await authUser(data)
+        } catch (error) {
+            msgText = error.response.data.message
+            msgType = 'error'
+        }
+
+        setFlashMessage(msgText, msgType)
+    }
+
+    async function login(user) {
+        let msgText = 'Login realizado com sucesso'
+        let msgType = 'success'
+
+        try {
+            const data = await api.post('/users/login', user).then((response) => {
                 return response.data
             })
 
@@ -56,5 +74,5 @@ export default function useAuth() {
         setFlashMessage(msgText, msgType)
     }
 
-    return { authenticated, register, logout }
+    return { authenticated, register, logout, login }
 }
