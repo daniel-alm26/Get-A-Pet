ce5d497b8e2516b0acebe93fcb7bf6f4af81d1d0
Daniel Cardoso <cardoso.daniel2612@gmail.com>
2023-05-16 11:35:28 -0300

    Atualização do usuário

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -7,9 +7,12 @@ import formStyles from '../../form/Form.module.css'
 
 import Input from '../../form/Input'
 
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
 function Profile() {
     const [user, setUser] = useState({})
     const [token] = useState(localStorage.getItem('token') || '')
+    const { setFlashMessage } = useFlashMessage()
 
     useEffect(() => {
 
@@ -18,22 +21,53 @@ function Profile() {
                 Authorization: `Bearer ${JSON.parse(token)}`
             },
         })
-        .then((response) => {
-            setUser(response.data)
-        })
+            .then((response) => {
+                setUser(response.data)
+            })
     }, [token])
 
-    function onFileChange(e) { }
+    function onFileChange(e) {
+        setUser({ ...user, [e.target.name]: e.target.files[0] })
+    }
 
-    function handleChange(e) { }
+    function handleChange(e) {
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        let msgType = 'success'
+
+        const formData = new FormData()
+
+        await Object.keys(user).forEach((key) => formData.append(key, user[key]))
+
+        const data = await api
+            .patch(`/users/edit/${user._id}`, formData, {
+                headers: {
+                    Authorization: `Bearer ${JSON.parse(token)}`,
+                    'Content-Type': 'multipart/form-data',
+                },
+            })
+            .then((response) => {
+                return response.data
+            })
+            .catch((err) => {
+                msgType = 'error'
+                return err.response.data
+            })
+
+        setFlashMessage(data.message, msgType)
+    }
 
     return (
         <section>
             <div className={styles.profile_header}>
                 <h1>Perfil</h1>
                 <p>Preview Imagem</p>
-            </div>            
-            <form className={formStyles.form_container}>
+            </div>
+            <form onSubmit={handleSubmit} className={formStyles.form_container}>
                 <Input
                     text="Imagem"
                     type="file"
