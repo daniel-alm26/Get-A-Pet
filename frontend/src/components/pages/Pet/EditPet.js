71da0e040f428496a86b9f213f5a4e3c8e4918b2
Daniel Cardoso <cardoso.daniel2612@gmail.com>
2023-05-20 17:44:04 -0300

    Preenchendo formulário de edição de dados

--- frontend/src/components/pages/Pet/EditPet.js
+++ frontend/src/components/pages/Pet/EditPet.js
@@ -1,6 +1,7 @@
 import api from "../../../utils/api"
 
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from "react-router-dom"
 
 import styles from './AddPet.module.css'
 
@@ -9,13 +10,35 @@ import PetForm from "../../form/PetForm"
 /* hooks */
 import useFlashMessage from "../../../hooks/useFlashMessage"
 
-function EditPet () {
+function EditPet() {
+    const [pet, setPet] = useState({})
+    const [token] = useState(localStorage.getItem('token') || '')
+    const { id } = useParams()
+    const { setFlashMessage } = useFlashMessage()
+
+    useEffect(() => {
+        api
+            .get(`/pets/${id}`, {
+                Authorization: `Bearer ${JSON.parse(token)}`
+            })
+            .then((response) => {
+                setPet(response.data.pet)
+            })
+    }, [token, id])
+
+    async function updatePet(pet) {
+
+    }
+
     return (
         <section>
             <div className={styles.addpet_header}>
-                <h1>Editando o Pet: 'pet.name'</h1>
+                <h1>Editando o Pet: {pet.name}</h1>
                 <p>Depois da edição os dados serão atualizados no sistema</p>
             </div>
+            {pet.name && (
+                <PetForm handleSubmit={updatePet} btnText="Atualizar" petData={pet} />
+            )}
         </section>
     )
 }
