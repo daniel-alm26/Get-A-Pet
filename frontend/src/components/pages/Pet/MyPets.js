4d4fcde08f09a06391e64c2c6c2dd93dde1d6c3b
Daniel Cardoso <cardoso.daniel2612@gmail.com>
2023-05-29 23:01:59 -0300

    Conclusão de adoção

--- frontend/src/components/pages/Pet/MyPets.js
+++ frontend/src/components/pages/Pet/MyPets.js
@@ -50,6 +50,23 @@ function MyPets() {
         setFlashMessage(data.message, msgType)
     }
 
+    async function concludeAdoption(id) {
+        let msgType = 'success'
+
+        const data = await api.patch(`/pets/conclude/${id}`, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+            },
+        }).then((response) => {
+            return response.data
+        }).catch((err) => {
+            msgType = 'error'
+            return err.response.data
+        })
+
+        setFlashMessage(data.message, msgType)
+    }
+
     return (
         <section>
             <div className={styles.petlist_header}>
@@ -69,7 +86,17 @@ function MyPets() {
                             <div className={styles.actions}>
                                 {pet.available ? (
                                     <>
-                                        {pet.adopter && <button className={styles.conclude_btn}>Concluir adoção</button>}
+                                        {pet.adopter && (
+                                            <button
+                                                className={styles.conclude_btn}
+                                                onClick={() => {
+                                                    concludeAdoption(pet._id)
+
+                                                }}
+                                            >
+                                                Concluir adoção
+                                            </button>
+                                        )}
                                         <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
                                         <button onClick={() => {
                                             removePet(pet._id)
